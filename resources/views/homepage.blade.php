c3ee06ac7a889bc9a5aa811de4819d8b9989ab78
Federica Pozzer <federica.pozzer@gmail.com>
2023-03-29 16:59:28 +0200

    boh

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,1 +1,35 @@
-<h1>CIAO</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Laravel Model e Controller</title>
+
+    {{-- Bootstrap --}}
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container">
+        @if(isset($movies))
+        <h1>MOVIES:</h1>
+
+        <div class="row row-cols-5">
+            @foreach($movies as $movie)
+            <div class="col">
+
+                <div class="card text-bg-secondary mb-3" style="max-width: 18rem;">
+                    <div class="card-header">Header</div>
+                    <div class="card-body">
+                        <h5 class="card-title">Secondary card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                </div>
+            </div>
+            @endforeach
+        </div>
+        @else <div class="mt-5">nessun film trovato :( </div> 
+        @endif
+    </div>
+</body>
+</html>
