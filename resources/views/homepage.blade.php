90de9776b1d5444ae1d40d1bfa2eebf219c9b87c
Federica Pozzer <federica.pozzer@gmail.com>
2023-03-30 10:20:49 +0200

    done done!

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -4,15 +4,22 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Laravel Model e Controller</title>
+    <title>{{ env("APP_NAME") }}</title>
 
     {{-- Bootstrap --}}
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
+    {{-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous"> --}}
+    {{-- kikka, genio.. Bootstrap non serve, lo trovi in @vite('resources/js/app.js') !!!! --}}
+
+    @vite('resources/js/app.js')
+
+    {{-- Bootstrap icons --}}
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
+
 </head>
 <body>
     <div class="container">
         @if(isset($movies))
-        <h1 class="mt-3">MOVIES:</h1>
+        <h1 class="mt-5 text-light"> <b>MOVIES:</b></h1>
 
         <div class="row row-cols-5">
             @foreach($movies as $movie)
@@ -30,7 +37,7 @@
             </div>
             @endforeach
         </div>
-        @else <div class="mt-5">nessun film trovato :( </div> 
+        @else <div class="mt-5">Nessun film trovato <i class="bi bi-emoji-frown"></i> </div> 
         @endif
     </div>
 </body>
